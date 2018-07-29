803580f1294acb8629759c2437cccab798843d0f
Riki Agusnaidi <r.agusnaidi@gmail.com>
2018-07-29 11:51:44 +0700

    File php

---
+++ helloworld.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Hello</title>
+</head>
+<body>
+  <h1>PHP IS AWSOME</h1>
+  <H2>Today is <?= date('y-m-d') ?></H2>
+</body>
+</html>
